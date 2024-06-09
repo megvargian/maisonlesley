786c1d82dc15f7c3e7301ec893816c26f9155a76
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-06-09 17:38:13 +0300

    add test search

--- search.php
+++ search.php
@@ -8,6 +8,33 @@
  */
 
 get_header();
+
+$args = array(
+	'post_type'      => 'product', // WooCommerce products are stored as 'product'
+	'post_status'    => 'publish', // Only show published products
+	'posts_per_page' => -1, // Number of products to return (-1 for all)
+	's'              => $get_search_query,
+);
+
+// Create a new query
+$query = new WC_Product_Query( $args );
+
+// Get the products
+$products = $query->get_products();
+
+// Loop through the products and display their information
+foreach ( $products as $product ) {
+	// Get product object
+	$product_obj = wc_get_product( $product->get_id() );
+
+	// Display product information
+	echo 'Product ID: ' . $product_obj->get_id() . '<br>';
+	echo 'Product Name: ' . $product_obj->get_name() . '<br>';
+	echo 'Product Price: ' . $product_obj->get_price() . '<br>';
+	echo 'Product SKU: ' . $product_obj->get_sku() . '<br>';
+	echo 'Product Stock Status: ' . $product_obj->get_stock_status() . '<br>';
+	echo '<br>';
+}
 ?>
 <section class="single_search">
 	<div class="container">
