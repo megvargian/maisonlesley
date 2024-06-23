839644e0a90af5d61b55513b212b7578fede41d3
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-06-23 12:02:22 +0300

    test

--- template-parts/side-menu.php
+++ template-parts/side-menu.php
@@ -2,18 +2,14 @@
 $getGeneralFields = get_fields('options');
 $side_menu = $getGeneralFields['side_menu'];
 ?>
-<pre><?php print_r($side_menu); ?></pre>
+<pre><?php print_r($side_menu[0]['page'] -> ID); ?></pre>
 <div class="side-menu">
     <nav>
         <ul>
             <?php foreach( $side_menu as $page){ ?>
-                <pre><?php print_r($page['post_name']); ?></pre>
-                <pre><?php print_r($page['ID']); ?></pre>
-                <pre><?php echo $page -> post_name; ?></pre>
-                <pre><?php echo $page -> ID; ?></pre>
                 <li>
-                    <a class="<?=is_page($page -> post_name) ? 'active': '';?>" href="<?php echo get_permalink($page -> ID); ?>">
-                        <?php echo get_the_title($page -> ID) ?>
+                    <a class="<?=is_page($page['page'] -> post_name) ? 'active': '';?>" href="<?php echo get_permalink($page['page'] -> ID); ?>">
+                        <?php echo get_the_title($page['page'] -> ID) ?>
                     </a>
                 </li>
             <?php } ?>
