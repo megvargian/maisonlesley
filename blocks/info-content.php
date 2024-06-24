a2f6a99f9305502cc3023a6234a9d691c26ffb10
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-06-24 20:43:40 +0300

    fix accordion in info content block

--- blocks/info-content.php
+++ blocks/info-content.php
@@ -4,17 +4,18 @@
  */
 $info_content_block_fields = get_fields();
 $main_content = $info_content_block_fields['main_content'];
+$count = 0;
 ?>
 <section class="">
     <div class="accordion w-100" id="accordionExample">
-        <?php foreach($main_content as $content){ ?>
+        <?php foreach($main_content as $content){ $count++?>
             <div class="accordion-item">
                 <h2 class="accordion-header mt-0" id="headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne-<?php echo $count; ?>" aria-expanded="true" aria-controls="collapseOne-<?php echo $count; ?>">
                         <?php echo $content['title']; ?>
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne-<?php echo $count; ?>" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         <?php echo $content['single_content']; ?>
                     </div>
