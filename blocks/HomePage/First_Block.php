be0e35f0b99c7df7434873d05d759ab0a88a77e4
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-04 20:06:52 +0300

    add second section fix

--- blocks/HomePage/First_Block.php
+++ blocks/HomePage/First_Block.php
@@ -3,15 +3,17 @@
  * HomePage First Block Template
  */
 $homepage_first_block_fields = get_fields();
+$count = 0;
 ?>
 <section>
     <div class="container">
         <div class="row">
             <div class="swiper Mainswiper">
                 <div class="swiper-wrapper">
-                    <?php foreach($homepage_first_block_fields['images'] as $single_image){ ?>
+                    <?php foreach($homepage_first_block_fields['images'] as $single_image){
+                        $count++;?>
                         <div class="swiper-slide">
-                            <img class="w-100 px-0" src="<?php echo $single_image['image']; ?>/inc/assets/images/first-img.jpg" alt="main-image">
+                            <img class="w-100 px-0" src="<?php echo $single_image['image']; ?>" alt="main-image-<?php echo $count; ?>">
                         </div>
                     <?php } ?>
                 </div>
--- blocks/HomePage/Second_Block.php
+++ blocks/HomePage/Second_Block.php
@@ -34,19 +34,21 @@ $mutli_fields = $homepage_second_block_fields['multi_field'];
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-10 bg-white px-0">
-                <div class="row gx-0">
-                    <div class="col-3 px-0">
-                        <img class="w-100 h-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/second-img-mobile.jpg" alt="">
-                    </div>
-                    <div class="col px-0 d-flex justify-content-start align-items-center position-relative">
-                        <div class="d-block">
-                            <div class="">
-                                <h3 style="padding-left: 13px;">Yours new forever dress</h3>
+                <a href="<?php echo $single_field['button_url']; ?>">
+                    <div class="row gx-0">
+                        <div class="col-3 px-0">
+                            <img class="w-100 h-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/second-img-mobile.jpg" alt="">
+                        </div>
+                        <div class="col px-0 d-flex justify-content-start align-items-center position-relative">
+                            <div class="d-block">
+                                <div class="">
+                                    <h3 style="padding-left: 13px;"><?php echo $single_field['title']; ?></h3>
+                                </div>
+                                <div class="arrow-right"></div>
                             </div>
-                            <div class="arrow-right"></div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>
@@ -58,16 +60,20 @@ $mutli_fields = $homepage_second_block_fields['multi_field'];
                 <div class="row">
                     <div class="col-sm-6 col-12">
                         <div>
-                            <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/third.jpg" alt="third">
-                            <h3 class="pt-3">An Elgant Affair</h3>
-                            <a href="#" class="main-link mt-4">Discover more</a>
+                            <img class="w-100" src="<?php echo $mutli_fields[0]['image']; ?>" alt="<?php echo $mutli_fields[0]['title']; ?>">
+                            <h3 class="pt-3"><?php echo $mutli_fields[0]['title']; ?></h3>
+                            <a href="<?php echo $mutli_fields[0]['button_url']; ?>" class="main-link mt-4">
+                                <?php echo $mutli_fields[0]['button_text']; ?>
+                            </a>
                         </div>
                     </div>
                     <div class="col-sm-6 col-12">
                         <div class="pt-sm-5 mt-5 pt-0">
-                            <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/fourth.jpg" alt="fourth">
-                            <h3 class="pt-3">Perfect parters</h3>
-                            <a href="#" class="main-link mt-4">Discover more</a>
+                        <img class="w-100" src="<?php echo $mutli_fields[1]['image']; ?>" alt="<?php echo $mutli_fields[1]['title']; ?>">
+                            <h3 class="pt-3"><?php echo $mutli_fields[1]['title']; ?></h3>
+                            <a href="<?php echo $mutli_fields[1]['button_url']; ?>" class="main-link mt-4">
+                                <?php echo $mutli_fields[1]['button_text']; ?>
+                            </a>
                         </div>
                     </div>
                 </div>
