4c0d755f5b02871e5f2c72dde4a06f5cf2056915
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-06 18:26:28 +0300

    add mystique rose page desktop

--- mystiquerose-page.php
+++ mystiquerose-page.php
@@ -4,94 +4,163 @@
  */
 get_header();
 ?>
-<section>
-    <div class="container-fluid px-0">
-        <div class="row">
-            <div class="col-6" style="background-color: #D0212F;">
-
-            </div>
-            <div class="col-6 px-0">
-                <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/main-img-half-mystique-rose.jpg" alt="">
+<section class="mystique-rose-page">
+    <section class="py-5" style="background-color: #f5f5f5;">
+        <div class="container">
+            <div class="row text-center">
+                <h5>TODAY ONLY</h5>
+                <h3>Additional 10% off your Purchase</h3>
+                <p>Online only. Exclusion apply. <a href="#">Details</a></p>
+                <a href="#">Shop Now</a>
             </div>
         </div>
-    </div>
-</section>
-<section>
-    <div class="container">
-        <div class="row text-center py-4">
-            <h4>Going fast</h4>
-        </div>
-        <div class="row">
-            <div class="swiper swiperProduct">
-                <div class="swiper-wrapper">
-                    <?php
-                        for($i=1; $i<13; $i++){
-                        ?>
-                        <div class="swiper-slide">
-                            <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/product-slider/slider-<?php echo $i; ?>.jpg" alt="example-product">
-                            <h4>Flowing cady trousers</h4>
-                        </div>
-                    <?php
-                        }
-                    ?>
+    </section>
+    <section>
+        <div class="container-fluid px-0">
+            <div class="row">
+                <div class="col-6 px-0">
+                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/main-img-half-mystique-rose.jpg" alt="">
                 </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-            </div>
-        </div>
-    </div>
-</section>
-<section>
-    <a href="#" class="d-flex" style="height: fit-content;">
-        <img class="w-100" src="<?php echo get_template_directory_uri( ); ?>/inc/assets/images/second-img-mystique-rose.avif" alt="">
-    </a>
-</section>
-<section>
-    <a href="#" class="d-flex" style="height: fit-content;">
-        <img class="w-100" src="<?php echo get_template_directory_uri( ); ?>/inc/assets/images/home-tote-mystique-rose.avif" alt="">
-    </a>
-</section>
-<section>
-    <a href="#" class="d-flex" style="height: fit-content;">
-        <img class="w-100" src="<?php echo get_template_directory_uri( ); ?>/inc/assets/images/home-swim-mystique-rose.avif" alt="">
-    </a>
-</section>
-<section>
-    <a href="#" class="d-flex" style="height: fit-content;">
-        <img class="w-100" src="<?php echo get_template_directory_uri( ); ?>/inc/assets/images/home-blue-mystique-rose.avif" alt="">
-    </a>
-</section>
-<section>
-    <div class="container">
-        <div class="row text-center py-4">
-            <h4>Going fast</h4>
-        </div>
-        <div class="row">
-            <div class="swiper swiperProduct">
-                <div class="swiper-wrapper">
-                    <?php
-                        for($i=1; $i<13; $i++){
-                        ?>
-                        <div class="swiper-slide">
-                            <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/product-slider/slider-<?php echo $i; ?>.jpg" alt="example-product">
-                            <h4>Flowing cady trousers</h4>
+                <div class="col-6 d-flex justify-content-center align-items-center" style="background-color: #D0212F;">
+                    <div class="row justify-content-center d-flex align-items-center">
+                        <div class="col-12 justify-content-center text-center pb-4">
+                            <h6>THIS WEEKEND ONLY</h6>
+                            <h2>10/$40</h2>
+                            <h5>Panties</h5>
+                            <p>Orig up to 1495 each. <a href="#">Details</a></p>
                         </div>
-                    <?php
-                        }
-                    ?>
+                        <div class="col-6">
+                            <div class="row justify-content-center d-flex align-items-center">
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">Shop all</a>
+                                </div>
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                                <div class="col-12 d-flex justify-content-center align-items-center text-center px-1">
+                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
             </div>
         </div>
-    </div>
+    </section>
+    <section class="py-4">
+        <div class="container">
+            <div class="row text-center py-4">
+                <h3>Going fast</h3>
+            </div>
+            <div class="row justify-content-center">
+            <div class="col-11 px-0" style="position: relative;">
+                    <div class="swiper swiperProduct">
+                        <div class="swiper-wrapper">
+                            <?php
+                                for($i=1; $i<13; $i++){
+                                ?>
+                                <div class="swiper-slide">
+                                    <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/product-slider/slider-<?php echo $i; ?>.jpg" loading="lazy" alt="example-product">
+                                    <div class="swiper-lazy-preloader"></div>
+                                    <h4>Flowing cady trousers</h4>
+                                </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="swiper-button-prev" style="background-color: rgb(244, 235, 221) !important;"></div>
+                    <div class="swiper-button-next" style="background-color: rgb(244, 235, 221) !important;"></div>
+            </div>
+            </div>
+        </div>
+    </section>
+    <section class="background-img-section" style="background-image: url('<?php echo get_template_directory_uri( ); ?>/inc/assets/images/home-swim-mystique-rose.avif');">
+        <div class="container">
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-6 justify-content-center py-5 text-center">
+                    <h5>TODAY ONLY</h5>
+                    <h3>Additional 10% off your Purchase</h3>
+                    <p>Online only. Exclusion apply. <a href="#">Details</a></p>
+                    <a href="#">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="background-img-section-second" style="background-image: url('<?php echo get_template_directory_uri( ); ?>/inc/assets/images/sexy-tee.jpg');">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 justify-content-center py-5 text-center">
+                    <h5>TODAY ONLY</h5>
+                    <h3>Additional 10% off your Purchase</h3>
+                    <p>Online only. Exclusion apply. <a href="#">Details</a></p>
+                    <a href="#">Shop Now</a>
+                </div>
+                <div class="col-6"></div>
+            </div>
+        </div>
+    </section>
+    <section class="background-img-section-second" style="background-image: url('<?php echo get_template_directory_uri( ); ?>/inc/assets/images/cowgirl-control.avif');">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 justify-content-center py-5 text-center">
+                    <h5>TODAY ONLY</h5>
+                    <h3>Additional 10% off your Purchase</h3>
+                    <p>Online only. Exclusion apply. <a href="#">Details</a></p>
+                    <a href="#">Shop Now</a>
+                </div>
+                <div class="col-6"></div>
+            </div>
+        </div>
+    </section>
+    <section class="py-4">
+        <div class="container">
+            <div class="row text-center py-4">
+                <h3>Going fast</h3>
+            </div>
+            <div class="row justify-content-center">
+            <div class="col-11 px-0" style="position: relative;">
+                    <div class="swiper swiperProduct">
+                        <div class="swiper-wrapper">
+                            <?php
+                                for($i=1; $i<13; $i++){
+                                ?>
+                                <div class="swiper-slide">
+                                    <img class="w-100" src="<?php echo get_template_directory_uri() ?>/inc/assets/images/product-slider/slider-<?php echo $i; ?>.jpg" loading="lazy" alt="example-product">
+                                    <div class="swiper-lazy-preloader"></div>
+                                    <h4>Flowing cady trousers</h4>
+                                </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="swiper-button-prev" style="background-color: rgb(244, 235, 221) !important;"></div>
+                    <div class="swiper-button-next" style="background-color: rgb(244, 235, 221) !important;"></div>
+            </div>
+            </div>
+        </div>
+    </section>
 </section>
 <script>
     jQuery(document).ready(function($) {
         let swiper = new Swiper(".swiperProduct", {
             slidesPerView: 5,
             spaceBetween: 30,
-            loop: true,
+            loop: false,
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
