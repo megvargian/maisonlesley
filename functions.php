e98127ae76b425a87920ef708cf945f249c54013
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-14 17:52:25 +0300

    test

--- functions.php
+++ functions.php
@@ -432,7 +432,7 @@ function custom_product_filter() {
                         cat_id: '<?php echo $category->term_id; ?>'
                     },
                     success: function(response) {
-                        $('#product-list').html(response);
+                        $('#product-list .contianer .row').html(response);
                     }
                 });
             });
