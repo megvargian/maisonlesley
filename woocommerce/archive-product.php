05d978633547ccad4d1c516703136eb75b75a042
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-16 00:51:29 +0300

    test

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -69,7 +69,7 @@ global $counter_products;
 
 						$posts = []; // Initialize an array to hold the posts
 						// Populate the array with posts
-						$current_index = 0;
+						$current_index = 1;
 						while ( have_posts() ) {
 							the_post();
 							array_push($posts, get_post());
