9f094e09f6f1d8addb5c14a3f79cc84a34a15a7f
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-20 12:27:10 +0300

    test

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -62,9 +62,6 @@ if ( post_password_required() ) {
                  * @hooked woocommerce_template_single_price - 10
                  * @hooked woocommerce_template_single_excerpt - 20
                  * @hooked woocommerce_template_single_add_to_cart - 30
-                 * @hooked woocommerce_template_single_meta - 40
-                 * @hooked woocommerce_template_single_sharing - 50
-                 * @hooked WC_Structured_Data::generate_product_data() - 60
                  */
                 do_action( 'woocommerce_single_product_summary' );
                 ?>
