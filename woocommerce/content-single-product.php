141026be087b54be26b58a587b5b82325c0065d1
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-20 12:45:24 +0300

    test

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -41,32 +41,36 @@ if ( post_password_required() ) {
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 	<div class="container">
         <div class="row">
-            <?php
-            /**
-             * Hook: woocommerce_before_single_product_summary.
-             *
-             * @hooked woocommerce_show_product_sale_flash - 10
-             * @hooked woocommerce_show_product_images - 20
-             */
-            do_action( 'woocommerce_before_single_product_summary' );
-            ?>
-
-            <div class="summary entry-summary">
+            <div class="col-md-6 col-12">
                 <?php
                 /**
-                 * Hook: woocommerce_single_product_summary.
+                 * Hook: woocommerce_before_single_product_summary.
                  *
-                 * @hooked woocommerce_template_single_title - 5
-                 * @hooked woocommerce_template_single_rating - 10
-                 * @hooked woocommerce_template_single_price - 10
-                 * @hooked woocommerce_template_single_excerpt - 20
-                 * @hooked woocommerce_template_single_add_to_cart - 30
-                 * @hooked woocommerce_template_single_meta - 40
-                 * @hooked woocommerce_template_single_sharing - 50
-                 * @hooked WC_Structured_Data::generate_product_data() - 60
+                 * @hooked woocommerce_show_product_sale_flash - 10
+                 * @hooked woocommerce_show_product_images - 20
                  */
-                do_action( 'woocommerce_single_product_summary' );
+                do_action( 'woocommerce_before_single_product_summary' );
                 ?>
+            </div>
+
+            <div class="col-md-6 col-12">
+                <div class="summary entry-summary">
+                    <?php
+                    /**
+                     * Hook: woocommerce_single_product_summary.
+                     *
+                     * @hooked woocommerce_template_single_title - 5
+                     * @hooked woocommerce_template_single_rating - 10
+                     * @hooked woocommerce_template_single_price - 10
+                     * @hooked woocommerce_template_single_excerpt - 20
+                     * @hooked woocommerce_template_single_add_to_cart - 30
+                     * @hooked woocommerce_template_single_meta - 40
+                     * @hooked woocommerce_template_single_sharing - 50
+                     * @hooked WC_Structured_Data::generate_product_data() - 60
+                     */
+                    do_action( 'woocommerce_single_product_summary' );
+                    ?>
+                </div>
             </div>
         </div>
         <div class="row">
