0d9283a3c7902e29c1dfc48627c70c7cf0244715
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-30 17:23:21 +0300

    test array

--- blocks/HomePage/Third_Block.php
+++ blocks/HomePage/Third_Block.php
@@ -16,10 +16,17 @@ $homepage_third_block_fields = get_fields();
                     <div class="swiper-wrapper">
                         <?php
                             foreach($homepage_third_block_fields['products'] as $single_product){
+                                echo '<div class="post-item">';
+                                echo '<h2>Title: ' . esc_html($single_product->post_title) . '</h2>';
+                                echo '<p>ID: ' . esc_html($single_product->ID) . '</p>';
+                                echo '<p>Author: ' . esc_html(get_the_author_meta('display_name', $single_product->post_author)) . '</p>';
+                                echo '<p>Date: ' . esc_html($single_product->post_date) . '</p>';
+                                echo '<p>Status: ' . esc_html($single_product->post_status) . '</p>';
+                                echo '<p>Content: ' . wp_kses_post($single_product->post_content) . '</p>';
+                                echo '<p>Excerpt: ' . esc_html($single_product->post_excerpt) . '</p>';
+                                echo '<p>Permalink: <a href="' . esc_url($single_product->guid) . '">' . esc_url($single_product->guid) . '</a></p>';
+                                echo '</div>';
                         ?>
-                        <pre><?php print_r($single_product[0]); ?></pre>
-                        <pre><?php print_r($single_product[0]->ID); ?></pre>
-
                             <div class="swiper-slide">
                                 <img class="w-100" src="<?php echo get_the_post_thumbnail_url( $single_product->ID, 'medium' ); ?>" alt="<?php echo $single_product->post_title; ?>" loading="lazy">
                                 <div class="swiper-lazy-preloader"></div>
