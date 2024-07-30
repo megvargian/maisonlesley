cdc924644fe565bc566330d0bc79db25e5547c33
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-30 17:36:34 +0300

    test product id

--- blocks/HomePage/Third_Block.php
+++ blocks/HomePage/Third_Block.php
@@ -5,7 +5,6 @@
  */
 $homepage_third_block_fields = get_fields();
 ?>
-<pre><?php print_r($homepage_third_block_fields['products']) ?></pre>
 <section class="py-5 my-5 swiper-products-section">
     <div class="container">
         <div class="row text-center justify-content-center pb-3">
@@ -16,22 +15,13 @@ $homepage_third_block_fields = get_fields();
                 <div class="swiper swiperProduct">
                     <div class="swiper-wrapper">
                         <?php
-                            foreach($homepage_third_block_fields['products'] as $single_product){
-                                echo '<div class="post-item">';
-                                echo '<h2>Title: ' . esc_html($single_product->post_title) . '</h2>';
-                                echo '<p>ID: ' . esc_html($single_product->ID) . '</p>';
-                                echo '<p>Author: ' . esc_html(get_the_author_meta('display_name', $single_product->post_author)) . '</p>';
-                                echo '<p>Date: ' . esc_html($single_product->post_date) . '</p>';
-                                echo '<p>Status: ' . esc_html($single_product->post_status) . '</p>';
-                                echo '<p>Content: ' . wp_kses_post($single_product->post_content) . '</p>';
-                                echo '<p>Excerpt: ' . esc_html($single_product->post_excerpt) . '</p>';
-                                echo '<p>Permalink: <a href="' . esc_url($single_product->guid) . '">' . esc_url($single_product->guid) . '</a></p>';
-                                echo '</div>';
+                            foreach($homepage_third_block_fields['products'] as $single_product_ID){
+                                $title = get_the_title($single_product_ID);
                         ?>
                             <div class="swiper-slide">
-                                <img class="w-100" src="<?php echo get_the_post_thumbnail_url( $single_product->ID, 'medium' ); ?>" alt="<?php echo $single_product->post_title; ?>" loading="lazy">
+                                <img class="w-100" src="<?php echo get_the_post_thumbnail_url( $single_product_ID, 'medium' ); ?>" alt="<?php echo $title; ?>" loading="lazy">
                                 <div class="swiper-lazy-preloader"></div>
-                                <h4><?php echo $single_product->post_title; ?></h4>
+                                <h4><?php echo $title; ?></h4>
                             </div>
                         <?php
                             }
