a2fdae6f248e8612f48e1c7c51b6b04d8fd426ad
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-07-30 22:01:34 +0300

    test

--- blocks/MystiqueRosePage/First_Block.php
+++ blocks/MystiqueRosePage/First_Block.php
@@ -2,15 +2,17 @@
 /**
  * MystiqueRosePage First Block Template
  */
+$homepage_first_block_fields = get_fields();
 ?>
 
 <section class="py-5" style="background-color: #f5f5f5;">
     <div class="container">
         <div class="row text-center">
-            <h5>TODAY ONLY</h5>
+            <!-- <h5>TODAY ONLY</h5>
             <h3>Additional 10% off your Purchase</h3>
             <p>Online only. Exclusion apply. <a href="#">Details</a></p>
-            <a href="#">Shop Now</a>
+            <a href="#">Shop Now</a> -->
+            <?php echo $homepage_first_block_fields['main_text']; ?>
         </div>
     </div>
 </section>
