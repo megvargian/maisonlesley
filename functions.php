66c34121a9809a451b0d3a108b93e27579e8838a
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-01 16:45:06 +0300

    remove first block

--- functions.php
+++ functions.php
@@ -263,29 +263,6 @@ function my_acf_init_block_types()
                 'keywords'          => array('testimonial', 'quote'),
             )
         );
-        acf_register_block_type(
-            array(
-                'name'              => 'MystiqueRose First Block',
-                'title'             => __('MystiqueRose First Block'),
-                'description'       => __('MystiqueRose First Block'),
-                'render_template'   => 'blocks/MystiqueRosePage/First_Block.php ',
-                'category'          => 'formatting',
-                'icon'              => 'admin-comments',
-                'keywords'          => array('testimonial', 'quote'),
-            )
-        );
-        acf_register_block_type(
-            array(
-                'name'              => 'Block2',
-                'title'             => __('Block2'),
-                'description'       => __('This is the second Block of MystiqueRosePage'),
-                'render_template'   => 'blocks/MystiqueRosePage/Second_Block.php',
-                'category'          => 'formatting',
-                'icon'              => 'admin-comments',
-                'keywords'          => array('testimonial', 'quote'),
-            )
-        );
-
     }
 }
 
