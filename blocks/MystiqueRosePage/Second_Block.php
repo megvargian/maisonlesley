c8082b3c923004f21fc0eb8791a88804f1d933d6
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-01 17:51:15 +0300

    fix image block two

--- blocks/MystiqueRosePage/Second_Block.php
+++ blocks/MystiqueRosePage/Second_Block.php
@@ -8,17 +8,12 @@ $Mystique_rose_second_block_fields = get_fields();
         <div class="container-fluid px-sm-0">
             <div class="row">
                 <div class="col-md-6 col-12 px-0">
-                    <!-- <img class="w-100 px-0" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/main-img-half-mystique-rose.jpg" alt=""> -->
-                    <img class="w-100 px-0" src="<?php echo $Mystique_rose_second_block_fields['main-image']; ?>" alt="mystique-rose">
+                    <img class="w-100 px-0" src="<?php echo $Mystique_rose_second_block_fields['main_image']; ?>" alt="mystique-rose">
                 </div>
                 <div class="col-md-6 col-12 d-flex justify-content-center align-items-center" style="background-color: #D0212F;">
                     <div class="row py-3 py-md-0 justify-content-center d-flex align-items-center">
                         <div class="col-12 justify-content-center text-center pb-4">
                             <?php echo $Mystique_rose_second_block_fields['main_text']; ?>
-                            <!-- <h6>THIS WEEKEND ONLY</h6>
-                            <h2>10/$40</h2>
-                            <h5>Panties</h5>
-                            <p>Orig up to 1495 each. <a href="#">Details</a></p> -->
                         </div>
                         <div class="col-md-6 col-10">
                             <div class="row justify-content-center d-flex align-items-center">
@@ -38,25 +33,6 @@ $Mystique_rose_second_block_fields = get_fields();
                                     </div>
                                 <?php }
                                 }?>
-
-                                <!-- <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div>
-                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div>
-                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div>
-                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div>
-                                <div class="col-6 px-1 mb-2 d-flex justify-content-center align-items-center text-center">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div>
-                                <div class="col-12 px-1 d-flex justify-content-center align-items-center text-center ">
-                                    <a class="bg-white w-100 py-2 px-4" href="#">X-Small</a>
-                                </div> -->
                             </div>
                         </div>
                     </div>
