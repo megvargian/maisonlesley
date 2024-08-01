5de666bee4e574e8306e6566e5cc9ca88cd684c6
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-01 19:20:32 +0300

    test fifth block

--- blocks/MystiqueRosePage/Fifth_Block.php
+++ blocks/MystiqueRosePage/Fifth_Block.php
@@ -3,16 +3,18 @@
  * MystiqueRose Fifth Block Template
  */
 $Mystique_rose_fifth_block_fields = get_fields();
+// /inc/assets/images/sexy-tee.jpg
+// /inc/assets/images/home-mobile-sexy-tee.avif
 ?>
-
-<section class="background-img-section-second d-none d-md-flex" style="background-image: url('<?php echo get_template_directory_uri( ); ?>/inc/assets/images/sexy-tee.jpg');">
+<section class="background-img-section-second d-none d-md-flex" style="background-image: url('<?php echo $Mystique_rose_fifth_block_fields['main_image']; ?>');">
     <div class="container">
         <div class="row">
             <div class="col-6 justify-content-center py-5 text-center">
-                <h5>TODAY ONLY</h5>
+                <!-- <h5>TODAY ONLY</h5>
                 <h3>Additional 10% off your Purchase</h3>
                 <p>Online only. Exclusion apply. <a href="#">Details</a></p>
-                <a href="#">Shop Now</a>
+                <a href="#">Shop Now</a> -->
+                <?php echo $Mystique_rose_fifth_block_fields['main_text']; ?>
             </div>
             <div class="col-6"></div>
         </div>
@@ -21,17 +23,18 @@ $Mystique_rose_fifth_block_fields = get_fields();
 <section class="d-md-none d-block">
     <div class="container-fluid px-sm-0">
         <div class="row">
-            <img class="w-100 px-0" src="<?php echo get_template_directory_uri( ); ?>/inc/assets/images/home-mobile-sexy-tee.avif" alt="">
+            <img class="w-100 px-0" src="<?php echo $Mystique_rose_fifth_block_fields['main_image_mobile']; ?>">
         </div>
     </div>
 </section>
 <section class="py-4 d-md-none d-block">
     <div class="container">
         <div class="row text-center">
-            <h5>TODAY ONLY</h5>
+            <!-- <h5>TODAY ONLY</h5>
             <h3>Additional 10% off your Purchase</h3>
             <p>Online only. Exclusion apply. <a href="#">Details</a></p>
-            <a href="#">Shop Now</a>
+            <a href="#">Shop Now</a> -->
+            <?php echo $Mystique_rose_fifth_block_fields['main_text']; ?>
         </div>
     </div>
 </section>
