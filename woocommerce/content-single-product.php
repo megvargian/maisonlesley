5b818745099a2d35be9fc44a4732a59b4deba993
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-02 12:10:09 +0300

    test

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -80,12 +80,21 @@ if ( post_password_required() ) {
                      *
                      * @hooked woocommerce_output_product_data_tabs - 10
                      * @hooked woocommerce_upsell_display - 15
-                     * @hooked woocommerce_output_related_products - 20
                      */
                     do_action( 'woocommerce_after_single_product_summary' );
                     ?>
                 </div>
             </div>
+        </div>
+        <div class="row">
+        <?php
+        /**
+         * Hook: woocommerce_after_single_product_summary.
+         *
+         * @hooked woocommerce_output_related_products - 20
+         */
+        do_action( 'woocommerce_after_single_product_summary' );
+        ?>
         </div>
     </div>
 </div>
