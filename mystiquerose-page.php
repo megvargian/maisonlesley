bf4e964018fc741b6308faf099539a9f1663440c
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-02 11:19:13 +0300

    test swiper

--- mystiquerose-page.php
+++ mystiquerose-page.php
@@ -11,39 +11,5 @@ get_header();
         endwhile;
     ?>
 </section>
-<script>
-    jQuery(document).ready(function($) {
-        let swiper = new Swiper(".swiperProduct", {
-            slidesPerView: 5,
-            spaceBetween: 30,
-            loop: false,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            breakpoints: {
-				// when window width is >= 320px
-				320: {
-					slidesPerView: 2.5,
-					spaceBetween: 20
-				},
-				// when window width is >= 480px
-				480: {
-					slidesPerView: 2.5,
-					spaceBetween: 30
-				},
-				// when window width is >= 640px
-				640: {
-					slidesPerView: 3,
-					spaceBetween: 30
-				},
-				991: {
-					slidesPerView: 5,
-					spaceBetween: 30
-				}
-			}
-        });
-    });
-</script>
 <?php
 get_footer();
