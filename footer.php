3b4a34d50292bfe77dbf85d88aad69d4b880e200
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-04 18:23:34 +0300

    fix button

--- footer.php
+++ footer.php
@@ -253,8 +253,8 @@ $main_footer_fields = $getGeneralFields['footer_sub_menu'];
             });
         });
         $('.product-attributes li').on('click', function(){
-            $('.product-attributes li span').removeClass('active');
-            $(this).find('span').addClass('active');
+            $('.product-attributes li button').removeClass('active');
+            $(this).find('button').addClass('active');
         });
         // Apply FancyBox to product images
         $('a.fancybox').fancybox({
