9046783bef2ac6c0772142cf5d40db8c7e8f1055
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-06 23:16:32 +0300

    only display description

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -80,8 +80,13 @@ if ( post_password_required() ) {
                      * @hooked woocommerce_output_product_data_tabs - 10
                      * @hooked woocommerce_upsell_display - 15
                      */
-                    do_action( 'woocommerce_after_single_product_summary' );
+                    // do_action( 'woocommerce_after_single_product_summary' );
+                    $long_description = $product->get_description();
                     ?>
+                    <div>
+                        <h2>Description</h2>
+                        <p><?php echo $long_description; ?></p>
+                    </div>
                 </div>
             </div>
         </div>
