8c2f1949800b8f115be22f23ee6bfff9306ee516
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-06 16:30:38 +0300

    test8

--- header.php
+++ header.php
@@ -13,9 +13,8 @@
 if (!session_id()) {
     session_start();
 }
-$is_product_from_mystiquerose = isset($_SESSION['is_product_from_mystiquerose']) ? $_SESSION['is_product_from_mystiquerose'] : '';
-$is_product_from_mystiquerose = !is_product() ? $is_product_from_mystiquerose : '';
-echo $is_product_from_mystiquerose;
+$is_product_from_mystiquerose = isset($_SESSION['is_product_from_mystiquerose']) ? $_SESSION['is_product_from_mystiquerose'] : 0;
+$is_product_from_mystiquerose = !is_product() && is_page() ? $is_product_from_mystiquerose : 0;
 $all_generalFields = get_fields('options');
 $top_header_fields = $all_generalFields['top_header_fields'];
 $left_side_top_header_fields = $top_header_fields['left_side_top_header'];
