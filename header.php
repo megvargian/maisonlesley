77a285cf028cc9d8aeda48ac5b176aa95cdee50c
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-06 16:22:55 +0300

    test6

--- header.php
+++ header.php
@@ -10,11 +10,20 @@
  * @package WP_Bootstrap_Starter
  */
 // Start or resume the session
-if (!session_id()) {
-    session_start();
+// if (!session_id()) {
+//     session_start();
+// }
+// $is_product_from_mystiquerose = isset($_SESSION['is_product_from_mystiquerose']) ? $_SESSION['is_product_from_mystiquerose'] : '';
+// echo $is_product_from_mystiquerose;
+global $product;
+if (is_product() && !empty($product)) {
+    $is_product_from_mystiquerose = has_term(17, 'product_cat', $product->get_id()) ||
+                                    has_term(23, 'product_cat', $product->get_id()) ||
+                                    has_term(18, 'product_cat', $product->get_id()) ||
+                                    has_term(25, 'product_cat', $product->get_id()) ||
+                                    has_term(20, 'product_cat', $product->get_id());
+    echo $is_product_from_mystiquerose;
 }
-$is_product_from_mystiquerose = isset($_SESSION['is_product_from_mystiquerose']) ? $_SESSION['is_product_from_mystiquerose'] : '';
-echo $is_product_from_mystiquerose;
 $all_generalFields = get_fields('options');
 $top_header_fields = $all_generalFields['top_header_fields'];
 $left_side_top_header_fields = $top_header_fields['left_side_top_header'];
