9e47fe9a65831d2d63731a2770ace63afc4e9a2f
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-07 17:08:14 +0300

    test

--- header.php
+++ header.php
@@ -71,10 +71,10 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
                                 <h2 class="accordion-header" id="headingOne">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                         data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Accordion Item #1
+                                        Size guide table
                                     </button>
                                 </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse"
+                                <div id="collapseOne" class="accordion-collapse collapse show"
                                     aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <div class="sizeguide-table">
@@ -188,7 +188,7 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
                                 <h2 class="accordion-header" id="headingTwo">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                         data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Accordion Item #2
+                                        Test 1
                                     </button>
                                 </h2>
                                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
@@ -209,7 +209,7 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                         data-bs-target="#collapseThree" aria-expanded="false"
                                         aria-controls="collapseThree">
-                                        Accordion Item #3
+                                        Test 2
                                     </button>
                                 </h2>
                                 <div id="collapseThree" class="accordion-collapse collapse"
@@ -362,7 +362,7 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
                             <div class="right-side">
                                <ul class="d-flex">
                                     <li>
-                                        <a class="whish-list" href="/cart"></a>
+                                        <a class="whish-list" href="/wishlist"></a>
                                     </li>
                                     <li>
                                         <a class="add-to-cart" href="/cart"></a>
@@ -466,7 +466,7 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
                             <div class="right-side">
                                 <ul class="d-flex">
                                     <li>
-                                        <a class="whish-list" href="/cart"></a>
+                                        <a class="whish-list" href="/wishlist"></a>
                                     </li>
                                     <li>
                                         <a class="add-to-cart" href="/cart"></a>
