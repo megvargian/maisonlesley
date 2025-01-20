5d0f2e37e80bc37dc6b03d8b10c26364e1a6d988
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-20 22:12:17 +0200

    add pixel script

--- header.php
+++ header.php
@@ -41,6 +41,17 @@ if($_SERVER['REQUEST_URI'] == '/shop/'){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <?php wp_head(); ?>
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+        (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '592188410290511'); // Replace YOUR_PIXEL_ID with your actual Facebook Pixel ID
+        fbq('track', 'PageView');
+    </script>
 </head>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZYBKLLT4J"></script>
