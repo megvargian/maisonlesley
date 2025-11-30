65206912e480fdbb55bf1f2cf7e9bfd34444d5da
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-11-30 19:54:37 +0200

    fix header for single product

--- header.php
+++ header.php
@@ -161,7 +161,7 @@ if($_SERVER['REQUEST_URI'] == '/shop/'){
                         </div>
                     </div>
                 </section>
-                <section class="second-header" style="<?php echo $is_MystiqueRose ? 'top: 25%;' : ''; ?>">
+                <section class="second-header" style="<?php echo is_page(2409) ? 'top: 25%;' : ''; ?>">
                     <div class="container h-100 px-0">
                         <div class="row h-100">
                             <div class="col-md-4 col-12 h-100">
@@ -202,7 +202,7 @@ if($_SERVER['REQUEST_URI'] == '/shop/'){
                         </div>
                     </div>
                 </section>
-                <section class="bg-white main-nav-section" style="<?php echo $is_MystiqueRose ? 'transform: translateY(20%);' : ''; ?>">
+                <section class="bg-white main-nav-section" style="<?php echo is_page(2409); ? 'transform: translateY(20%);' : ''; ?>">
                     <div class="container px-0">
                         <nav class="text-left main-nav d-flex justify-content-between">
                             <ul class="d-flex justify-content-start main-menu-list">
