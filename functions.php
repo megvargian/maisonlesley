3bef3f90ba8b3564e93e0095e85a435548be2d00
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-08-05 17:48:41 +0300

    fix the filter on color select

--- functions.php
+++ functions.php
@@ -1002,7 +1002,6 @@ function add_custom_add_to_cart_button() {
                                     // Merge explicit sizes with sizes available for any color
                                     var availSizes = (colorToSizes[selectedColor] || []).concat(anyColorSizes);
                                     availSizes = availSizes.filter(function(v, i, a) { return a.indexOf(v) === i; });
-                                    console.log('availSizes', availSizes);
                                     if (availSizes.length > 0) {
                                         $('.product-attributes-size li button').each(function() {
                                             var sSlug = $(this).data('size-slug');
@@ -1010,10 +1009,6 @@ function add_custom_add_to_cart_button() {
                                                 $(this).addClass('out-of-stock').prop('disabled', true);
                                             }
                                         });
-                                        // Safety net: revert if slug mismatch caused all sizes to be filtered
-                                        if ($('.product-attributes-size li button:not(.out-of-stock)').length === 0) {
-                                            resetSizes();
-                                        }
                                     }
                                 });
 
